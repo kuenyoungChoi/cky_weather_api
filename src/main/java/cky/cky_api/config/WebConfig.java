90643503ad91252310ed2f66e84c8e76753e1d22
cky <cky@ilark.co.kr>
2024-02-02 11:17:15 +0900

    react cors test v_01

--- src/main/java/cky/cky_api/config/WebConfig.java
+++ src/main/java/cky/cky_api/config/WebConfig.java
@@ -2,19 +2,18 @@ package cky.cky_api.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("Authorization", "Content-Type")
-                .exposedHeaders("Custom-Header")
-                .allowCredentials(true)
-                .maxAge(3600);
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("OPTIONS","GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
     }
 }
